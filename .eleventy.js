62b5e42ef7875311726fc11197830c30d5415144
vsantiago <victor.santiagogonzale@viasat.com>
2024-09-01 12:37:21 +0100

    fix: generic

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
-const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const markdownIt = require("markdown-it")
+const markdownItAnchor = require("markdown-it-anchor")
 
 module.exports = function(eleventyConfig) {
   // Plugins
@@ -10,37 +10,35 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true)
 
   // Copy these static files to _site folder
-  eleventyConfig.addPassthroughCopy('src/assets')
-  eleventyConfig.addPassthroughCopy('src/manifest.json')
+  eleventyConfig.addPassthroughCopy("src/assets")
+  eleventyConfig.addPassthroughCopy("src/manifest.json")
+  eleventyConfig.addPassthroughCopy({
+    "humans.txt": "/humans.txt",
+    "robots.txt": "/robots.txt"
+  });
 
   // To create excerpts
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    excerpt_alias: 'post_excerpt',
-    excerpt_separator: '<!-- excerpt -->'
+    excerpt_alias: "post_excerpt",
+    excerpt_separator: "<!-- excerpt -->"
   })
 
   // To create a filter to determine duration of post
-  eleventyConfig.addFilter('readTime', (value) => {
+  eleventyConfig.addFilter("readTime", (value) => {
     const content = value
-    const textOnly = content.replace(/(<([^>]+)>)/gi, '')
+    const textOnly = content.replace(/(<([^>]+)>)/gi, "")
     const readingSpeedPerMin = 450
     return Math.max(1, Math.floor(textOnly.length / readingSpeedPerMin))
   })
 
-  // Put robots.txt in root
-  eleventyConfig.addPassthroughCopy({ 
-    "humans.txt": "/humans.txt",
-    "robots.txt": "/robots.txt" 
-  });
-
   // Enable us to iterate over all the tags, excluding posts and all
-  eleventyConfig.addCollection('tagList', collection => {
+  eleventyConfig.addCollection("tagList", collection => {
     const tagsSet = new Set()
     collection.getAll().forEach(item => {
       if (!item.data.tags) return
       item.data.tags
-        .filter(tag => !['posts', 'all'].includes(tag))
+        .filter(tag => !["posts", "all"].includes(tag))
         .forEach(tag => tagsSet.add(tag))
     })
     return Array.from(tagsSet).sort()
@@ -51,19 +49,19 @@ module.exports = function(eleventyConfig) {
     level: [1, 2], 
     permalink: markdownItAnchor.permalink.headerLink({ 
       safariReaderFix: true,
-      class: 'header-anchor',
+      class: "header-anchor",
     })
   })
-  eleventyConfig.setLibrary('md', md)
+  eleventyConfig.setLibrary("md", md)
 
   // asset_img shortcode
-  eleventyConfig.addLiquidShortcode('asset_img', (filename, alt) => {
+  eleventyConfig.addLiquidShortcode("asset_img", (filename, alt) => {
     return `<img class="my-4" src="/assets/img/posts/${filename}" alt="${alt}" />`
   })
 
   return {
     dir: {
-      input: 'src'
+      input: "src"
     }
   }
 }
